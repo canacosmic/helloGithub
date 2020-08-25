f0e843389cd1a13717a8486e15c3a880a72ff52a
Roy C <61641010+canacosmic@users.noreply.github.com>
2020-08-25 13:37:59 -0500

    Creating app

---
+++ helloGithubTry2/helloGithubTry2/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  helloGithubTry2
+//
+//  Created by Rodrigo Canalizo Macías on 25/08/20.
+//  Copyright © 2020 Rodrigo Canalizo Macías. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ helloGithubTry2/helloGithubTry2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloGithubTry2
+//
+//  Created by Rodrigo Canalizo Macías on 25/08/20.
+//  Copyright © 2020 Rodrigo Canalizo Macías. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
